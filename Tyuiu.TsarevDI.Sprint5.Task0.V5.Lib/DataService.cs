377bddc9131cc21c3e8d2f064a71526cffebb59c
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-11-14 16:07:59 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint5.Task0.V5.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.TsarevDI.Sprint5.Task0.V5.Lib
+{
+    public class DataService : ISprint5Task0V5
+    {
+        public string SaveToFileTextData(int x)
+        {
+            ///string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+
+            double y = Math.Round(Math.Log((x + 1) / (x + 2)), 3); 
+
+            File.WriteAllText(path, Convert.ToString(y));
+            return path;
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.TsarevDI.Sprint5.Task0.V5/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.TsarevDI.Sprint5.Task0.V5.Lib;
+namespace Tyuiu.TsarevDI.Sprint5.Task0.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+
+            Console.Title = "Спринт #5 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #5                                                             *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                      *");
+            Console.WriteLine("* Задание #0                                                            *");
+            Console.WriteLine("* Варинат #11                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+
+            int x = 5;
+            Console.WriteLine("x = 5");
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine("Файл: "+ds.SaveToFileTextData(x));
+            Console.WriteLine("Создан");
+
+
+
+        }
+    }
+}
