9c7b4a47c063aae6b5cf44578d249e7f37fd8064
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-11-24 21:44:40 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint5.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.TsarevDI.Sprint5.Task5.V27.Lib
+{
+    public class DataService : ISprint5Task5V27
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double t = 0;
+            int k = 0;
+           
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = line.Replace('.', ',');
+                    if (Convert.ToDouble(line) % 1 == 0)
+                    {
+                        t += Convert.ToDouble(line);
+                        k++;
+                    }
+                }
+              
+
+            }
+            return t/k;
+
+        } 
+    }
+}
---
+++ Tyuiu.TsarevDI.Sprint5.Task5.V27/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.TsarevDI.Sprint5.Task5.V27.Lib;
+namespace Tyuiu.TsarevDI.Sprint5.Task5.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #5                                                             *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                        *");
+            Console.WriteLine("* Задание #5                                                            *");
+            Console.WriteLine("* Варинат #27                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+
+            string path = @"C:\DataSprint5\input.txt";
+
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine("среднее значение файла = "+ds.LoadFromDataFile(path));
+        }
+    }
+}
