6d8d3981986c51d4223e710bf5d145a018eb7b54
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-11-25 22:42:02 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint5.Task6.V29.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.TsarevDI.Sprint5.Task6.V29.Lib
+{
+    public class DataService : ISprint5Task6V29
+    {
+        public int LoadFromDataFile(string path)
+        {
+            
+            int k;
+            k = 0;
+            
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    
+                    string[] array = line.Split(' ');
+
+                    foreach (string w in array)
+                    {
+                       if (w.Length == 7)
+                       {
+                            k++;
+                       }
+                 
+                    }
+
+                }
+            }
+            return k;
+        }
+    }
+    
+}       
---
+++ Tyuiu.TsarevDI.Sprint5.Task6.V29/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.TsarevDI.Sprint5.Task6.V29.Lib;
+namespace Tyuiu.TsarevDI.Sprint5.Task6.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #5                                                             *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                      *");
+            Console.WriteLine("* Задание #6                                                            *");
+            Console.WriteLine("* Варинат #29                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+
+            string path = @"D:\DataSprint5\input.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine("количество слов длиной семь символов = " + ds.LoadFromDataFile(path));
+        }
+    }
+}
