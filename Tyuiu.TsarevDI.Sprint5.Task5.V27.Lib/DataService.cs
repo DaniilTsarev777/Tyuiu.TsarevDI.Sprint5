cd289ad1d79c54609d42d54240eb899bdd9917b5
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-11-25 20:58:31 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint5.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.TsarevDI.Sprint5.Task5.V27.Lib
+{
+    public class DataService : ISprint5Task5V27
+    {
+        public double LoadFromDataFile(string path)
+        {
+            
+
+            string[] strings = path.Split(' ');
+            int k, s;
+            k = 0;
+            s = 0;
+
+            foreach (string str in strings)
+
+            {
+
+                if (int.TryParse(str.Trim(), out int number) && number % 1 == 0)
+
+                {
+                    s += Convert.ToInt32(number);
+                    k++;
+                }
+            }
+
+            return s/k; 
+
+        } 
+    }
+}
