c7867590e15c6fd128586fed8860cc395433d97a
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-11-26 20:14:01 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint5.Task7.V13/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.TsarevDI.Sprint5.Task7.V13.Lib;
+namespace Tyuiu.TsarevDI.Sprint5.Task7.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #5                                                             *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту               *");
+            Console.WriteLine("* Задание #7                                                            *");
+            Console.WriteLine("* Варинат #13                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+
+            string path = @"C:\DataSprint5\input.txt";
+            
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine("Находится в файле " + ds.LoadDataAndSave(path));
+            Console.ReadKey();
+        }
+    }
+}
